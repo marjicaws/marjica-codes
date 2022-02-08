35c7004f6c91fef84c6db8564e317990f9f83a81
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-02-08 16:52:26 -0500

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 const Home = () => {
   return <div className='home-img-div'>
     <div className="home-border">
-     <h1 className="home-name">Marjica Watson-Santos</h1>
+     <h1 className="home-name">Hi, I'm Marjica Watson-Santos.</h1>
      <h2 className="home-title">Software Engineer</h2>
      <h3 className="home-date">2022</h3>
      </div>
