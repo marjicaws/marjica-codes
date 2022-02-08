130d1a67a0870eaefb6162d08196dd53af931a51
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-02-08 18:05:46 -0500

    nav bar

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,10 +6,10 @@ export default function Project() {
   return (<div className='prj'>
     <h1 className='prj-title'>projects = [ </h1>
     <div className='prj-links'>
-     <h2 className='prj-1'><a class="float-start"href = "https://github.com/marjicaws/4-tune"><img className="prj-img"src ='https://res.cloudinary.com/marssantos/image/upload/v1642917093/4-tunepic_cdnjcq.png'/></a><h3 className = "prj-text">4-Tune</h3></h2>
-     <h2><a className='prj-2'class="float-start"href='https://github.com/marjicaws/The-Void-Reviews'><img className="prj-img"src ='https://res.cloudinary.com/marssantos/image/upload/v1642917107/voidpic_y7jcou.png'/></a><h3 className = "prj-text">The Void Reviews</h3></h2>
-     <h2><a className='prj-3'class="float-start"href='https://github.com/marjicaws/frntr-app'><img className="prj-img"src ='https://res.cloudinary.com/marssantos/image/upload/v1642917093/4-tunepic_cdnjcq.png'/></a><h3 className = "prj-text">Frntr</h3></h2>
-     <h2><a className='prj-3' class="float-start" href='https://github.com/marjicaws/listnr2'><img className="prj-img"src ='https://res.cloudinary.com/marssantos/image/upload/v1642917093/4-tunepic_cdnjcq.png'/></a><h3 className = "prj-text">Listnr</h3></h2>
+     <h2 ><a className='prj-4' class="float-start"href = "https://github.com/marjicaws/4-tune"><h3 className = "prj-text">4-Tune</h3></a></h2>
+     <h2><a className='prj-4'class="float-start"href='https://github.com/marjicaws/The-Void-Reviews'><h3 className = "prj-text">The Void Reviews</h3></a></h2>
+     <h2><a className='prj-4'class="float-start"href='https://github.com/marjicaws/frntr-app'><h3 className = "prj-text">Frntr</h3></a></h2>
+     <h2><a className='prj-4' class="float-start" href='https://github.com/marjicaws/listnr2'><h3 className = "prj-text">Listnr</h3></a></h2>
      </div>
 
      <h1 className='prj-end'>]</h1>
