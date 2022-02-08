651489fad78fca462fe43d9486ee75a3a2cbe3b0
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-02-08 17:21:52 -0500

    new logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,8 @@ import React from 'react';
 const Home = () => {
   return <div className='home-img-div'>
     <div className="home-border">
-     <h1 className="home-name">Hi, I'm Marjica Watson-Santos.</h1>
+      <img src = "https://res.cloudinary.com/marssantos/image/upload/v1644358299/Neutral_Script_Typography_Logo_2_ihseu6.png" />
+     <h1 data-text=" Marjica Watson-Santos" className="home-name"> Marjica Watson-Santos</h1>
      <h2 className="home-title">Software Engineer</h2>
      <h3 className="home-date">2022</h3>
      </div>
