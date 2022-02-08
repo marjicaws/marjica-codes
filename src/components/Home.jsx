a48b56d0a6ed98df33575f6adc80c5cf6706e93f
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-02-08 17:44:04 -0500

    nav bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,9 @@ import React from 'react';
 const Home = () => {
   return <div className='home-img-div'>
     <div className="home-border">
-      <img src = "https://res.cloudinary.com/marssantos/image/upload/v1644358299/Neutral_Script_Typography_Logo_2_ihseu6.png" />
-     <h1 data-text=" Marjica Watson-Santos" className="home-name"> Marjica Watson-Santos</h1>
+    <h1 data-text=" Marjica Watson-Santos" className="home-name"> Marjica Watson-Santos</h1>
+      <img className='image-rotate'src = "https://res.cloudinary.com/marssantos/image/upload/v1644358299/Neutral_Script_Typography_Logo_2_ihseu6.png" />
+    
      <h2 className="home-title">Software Engineer</h2>
      <h3 className="home-date">2022</h3>
      </div>
