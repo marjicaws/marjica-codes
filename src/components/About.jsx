bae6853c9e5fa18ba2ff106385cd7a967cf51c7d
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-06-17 20:40:10 -0400

    more cosmetics

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,28 +9,50 @@ export default function About() {
       <h2 className='about-title'> Get to know me:</h2>
       <ul className='about-detail'>
         <div className='general'>
+        <li><img src="https://res.cloudinary.com/marssantos/image/upload/v1655408695/IMG_8829_wpdj6o.jpg" height={600} className="self-img"></img></li>
+        <div className='general-box'>
         <li className='name'>Marjica Watson-Santos</li>
         <li className='location'> Atlanta, GA</li>
         <li className='title'>Aspiring Software Engineer, General Assembly 2022</li>
         <li className='title'>B.A. in Creative Writing with a  Music Minor, Georgia State University</li>
         <li className='title'>Former Opera Singer and Songwriter</li>
         </div>
+        </div>
+        </ul>
+
+       <div className='box-one'>
+       <ul>
+         <h3 className= 'bio-title'>Skills:</h3>
+        <li className='skills'>HTML</li>
+        <li className='skills'>CSS</li>
+        <li className='skills'>JS</li>
+        <li className='skills'>ReactJS</li>
+        <li className='skills'>Python</li>
+        <li className='skills'>Django</li>
+        <li className='skills'>MongoDB</li>
+        <li className='skills'>SQL</li>
+        <li className='skills'>Mongoose</li>
+        
+       
+       </ul>
+        
         
 
-       
-        <li className='skills'>HTML CSS JS React.Js Python Django MongoDb SQL Mongoose</li>
-
-      </ul>
+      </div>
+      <div className='box-two'>
+      <h3 className='bio-title'>Bio:</h3>
       <p className='about-paragraph'>Singing and songwriting taught me about the importance of being willing to go the extra mile for the finished product. 
         From pulling all-nighters to finish tracks, to pulling all-nighters to code and debug projects, a hard day&#39;s work 
-        has never thrown me off my rhythm. Creative and artistic, my talent and passion for music has intermingled with my
+        has never thrown me off my rhythm. 
+        
+        Creative and artistic, my talent and passion for music has intermingled with my
         love of writing beautiful, clean, and functional code. I pride myself on my ability to quickly adapt to change and use 
         my personal time to grow my skill set and hone my talents. I&#39;m an asset to any team I&#39;m on by being just as willing 
         to support as I am to lead. Adaptive, Multi-Disciplined, and Supportive, I strive to bring life to all of my creations.</p>
+        </div>
         <h2 className='about-end'></h2>
-        
-
-        <button  className='download-link'><a className='download-button' href = "resume.png" download="Marjica Watson-Santos Resume 2022">Download My Resume</a></button>
+      
+        <button  className='download-link'><a className='download-button' href = "/Users/marjicawatson-santos/velvetworms/private-projects/marjica-codes/marjica_codes/src/components/resume/resume.png" download="Marjica Watson-Santos Resume 2022">Download My Resume</a></button>
         </div>
   </div>
   )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,10 +6,22 @@ export default function Project() {
   return (<div className='prj'>
     <h1 className='prj-title'>projects : </h1>
     <div className='prj-links'>
+   
+    <div className="prj-box1">
      <h2 ><a className='prj-4' class="float-start"href = "https://github.com/marjicaws/4-tune"><h3 className = "prj-text">4-Tune</h3></a></h2>
+     </div>
+     
+     
+     <div className="prj-box2">
      <h2><a className='prj-4'class="float-start"href='https://github.com/marjicaws/The-Void-Reviews'><h3 className = "prj-text">The Void Reviews</h3></a></h2>
-     <h2><a className='prj-4'class="float-start"href='https://github.com/marjicaws/frntr-app'><h3 className = "prj-text">Frntr</h3></a></h2>
+     </div>
+     <div className="prj-box4">
      <h2><a className='prj-4' class="float-start" href='https://github.com/marjicaws/listnr2'><h3 className = "prj-text">Listnr</h3></a></h2>
+     </div>
+     <div className="prj-box3">
+     <h2><a className='prj-4'class="float-start"href='https://github.com/marjicaws/frntr-app'><h3 className = "prj-text">Frntr</h3></a></h2>
+     </div>
+    
      </div>
 
      <h1 className='prj-end'></h1>
